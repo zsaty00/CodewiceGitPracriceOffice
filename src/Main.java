b4ac5e1b30b43aede9630977673a65ca234ec545
zsaty00 <satylgan.dev@gmail.com>
2023-11-06 14:33:00 -0600

    Adding new project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String str = "Hello Batch 3 students!";
+        System.out.println(str);
+    }
+}
